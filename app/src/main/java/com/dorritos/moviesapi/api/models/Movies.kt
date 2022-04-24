1467c5d827c71e3e905ffd528f4633993718a538
Aidar <dorritos002@gmail.com>
2022-04-24 12:37:27 +0300

    initial

---
+++ app/src/main/java/com/dorritos/moviesapi/api/models/Movies.kt
@@ -0,0 +1,9 @@
+package com.dorritos.moviesapi.api.models
+
+data class Movies(
+    val copyright: String,
+    val has_more: Boolean,
+    val num_results: Int,
+    val results: List<Result>,
+    val status: String
+)
---
+++ app/src/main/java/com/dorritos/moviesapi/api/models/Multimedia.kt
@@ -0,0 +1,8 @@
+package com.dorritos.moviesapi.api.models
+
+data class Multimedia(
+    val height: Int,
+    val src: String,
+    val type: String,
+    val width: Int
+)
